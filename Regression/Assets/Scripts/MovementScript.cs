d35734972f190eff36dc4485774efd1e3326d7b9
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-11-03 15:09:43 +0000

    Modified Movement script
    I have modified the movement script to work correctly. The player now moves at a consistent speed in the editor.
    This still needs to be tested within the build.

--- Regression/Assets/Scripts/MovementScript.cs
+++ Regression/Assets/Scripts/MovementScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class MovementScript : MonoBehaviour
@@ -16,27 +17,30 @@ public class MovementScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(transform.position.x, 5, transform.position.z);
+        //transform.position = new Vector3(transform.position.x, 5, transform.position.z);
         //Movement using WASD
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed));
-
+            //rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed));
+            rb.velocity = transform.forward * movementSpeed;
             
             
 
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed * -1)); //multiplied by -1 to go opposite direction
+            //rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed * -1)); //multiplied by -1 to go opposite direction
+            rb.velocity = transform.forward * -movementSpeed;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            rb.AddRelativeForce(new Vector3(movementSpeed * -1, 0.0f, 0.0f));
+            //rb.AddRelativeForce(new Vector3(movementSpeed * -1, 0.0f, 0.0f));
+            rb.velocity = transform.right * -movementSpeed;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.AddRelativeForce(new Vector3(movementSpeed, 0.0f, 0.0f));
+            //rb.AddRelativeForce(new Vector3(movementSpeed, 0.0f, 0.0f));
+            rb.velocity = transform.right * movementSpeed;
         }
         else
         {
