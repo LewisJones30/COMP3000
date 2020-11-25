b9cb32ba9a528a01e44552f3817f43fa8848f1a2
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-11-25 12:53:07 +0000

    Modified movement script to work in multiple directions. Commented out code will be removed in the next build.

--- Regression/Assets/Scripts/MovementScript.cs
+++ Regression/Assets/Scripts/MovementScript.cs
@@ -24,33 +24,40 @@ public class MovementScript : MonoBehaviour
         }
         //transform.position = new Vector3(transform.position.x, 5, transform.position.z);
         //Movement using WASD
-        if (Input.GetKey(KeyCode.W))
-        {
-            //rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed));
-            rb.velocity = transform.forward * movementSpeed;
-            
-            
+        //    if (Input.GetKey(KeyCode.W))
+        //    {
+        //        //rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed));
+        //        if (Input.GetKey(KeyCode.A))
+        //        {
+        //            rb.velocity = (transform.forward + (-transform.right) / 2) * movementSpeed;
+        //        }
+        //        rb.velocity = transform.forward * movementSpeed;
 
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            //rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed * -1)); //multiplied by -1 to go opposite direction
-            rb.velocity = transform.forward * -movementSpeed;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            //rb.AddRelativeForce(new Vector3(movementSpeed * -1, 0.0f, 0.0f));
-            rb.velocity = transform.right * -movementSpeed;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            //rb.AddRelativeForce(new Vector3(movementSpeed, 0.0f, 0.0f));
-            rb.velocity = transform.right * movementSpeed;
-        }
-        else
-        {
-            rb.velocity = Vector3.zero;
-        }
+
+        float X = Input.GetAxisRaw("Horizontal");
+        float Z = Input.GetAxisRaw("Vertical");
+        rb.velocity = (transform.right * X + transform.forward * Z) * movementSpeed;
+    //    }
+    //    else if (Input.GetKey(KeyCode.S))
+    //    {
+    //        //rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed * -1)); //multiplied by -1 to go opposite direction
+    //        rb.velocity = transform.forward * -movementSpeed
+    //    }
+    //    else if (Input.GetKey(KeyCode.A))
+    //    {
+    //        //rb.AddRelativeForce(new Vector3(movementSpeed * -1, 0.0f, 0.0f));
+    //        rb.velocity = transform.right * -movementSpeed;
+    //    }
+    //    else if (Input.GetKey(KeyCode.D))
+    //    {
+    //        //rb.AddRelativeForce(new Vector3(movementSpeed, 0.0f, 0.0f));
+    //        rb.velocity = transform.right * movementSpeed;
+    //    }
+    //    else
+    //    {
+    //        rb.velocity = Vector3.zero;
+    //    }
+    //
     }
 
 }
