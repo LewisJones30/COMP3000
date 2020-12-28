5f7af7460591fe19a6641313b3f8c27aa73988e4
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-12-28 13:57:50 +0000

    Updated player movement script
    The player can now collide with enemies, I am going to update this to work with walls as well in the next push.
    The next push will also include a modification to the enemy movement speed so it is possible to toggle the movement speed for enemies in the prefab's inspector tab.

--- Regression/Assets/Scripts/MovementScript.cs
+++ Regression/Assets/Scripts/MovementScript.cs
@@ -10,6 +10,7 @@ public class MovementScript : MonoBehaviour
     public float rotationSpeed = 10; //Multiplier for rotation speed
     public Rigidbody rb;
     UIController ui;
+    RaycastHit hit;
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
@@ -24,7 +25,26 @@ public class MovementScript : MonoBehaviour
         }
         float X = Input.GetAxisRaw("Horizontal");
         float Z = Input.GetAxisRaw("Vertical");
-        rb.velocity = (transform.right * X + transform.forward * Z) * movementSpeed;
+
+        rb.AddRelativeForce(new Vector3(X * movementSpeed, 0, Z * movementSpeed), ForceMode.Force);
+        Vector3 newVelocity = rb.velocity.normalized;
+        newVelocity *= 1.5f;
+       rb.velocity = newVelocity;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, forward, out hit, 2))
+        {
+            if (hit.transform.gameObject.layer == 10)
+            {
+                rb.angularVelocity = Vector3.zero;
+                rb.velocity = Vector3.zero;
+            }
+        }
+        //rb.velocity = (transform.right * X + transform.forward * Z) * movementSpeed;
+        if (X + Z == 0)
+        {
+            rb.angularVelocity = Vector3.zero;
+            rb.velocity = Vector3.zero;
+        }
     }
 
 }
